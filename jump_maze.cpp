48dc154bc6c72523ac07fc14ab7f06071ee86b35
Dorian Haglund <dorian.haglund@gmail.com>
2018-09-11 07:54:25 +0200

    change the update function to solve the second half of the problem

--- jump_maze.cpp
+++ jump_maze.cpp
@@ -13,6 +13,14 @@ int jump(int head, Maze const & maze)
   return head + maze[head];
 }
 
+void update_maze(int idx, Maze & maze)
+{
+  if(maze[idx] >= 3)
+    maze[idx]--;
+  else
+    maze[idx]++;
+}
+
 int exit_maze(Maze const & maze_)
 {
   int steps = 0;
@@ -26,7 +34,7 @@ int exit_maze(Maze const & maze_)
 
     auto previous_head = head;
     head = jump(head, maze);
-    maze[previous_head]++;
+    update_maze(previous_head, maze);
 
     steps++;
   }
@@ -48,7 +56,7 @@ Maze read_maze(char const * path)
 int main()
 {
   assert(exit_maze({1}) == 1);
-  assert(exit_maze({0, 3, 0, 1, -3}) == 5);
+  assert(exit_maze({0, 3, 0, 1, -3}) == 10);
   
   auto maze = read_maze("maze.txt");
 
